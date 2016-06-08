679a55bae8a9ed44354fe78d046336834d17c438
CoPLaS <pavlasek@gmail.com>
2016-06-08 16:16:51 +0200

    detail of item example

--- src/main/java/com/example/controllers/DummyModelController.java
+++ src/main/java/com/example/controllers/DummyModelController.java
@@ -9,10 +9,11 @@ import org.springframework.data.querydsl.binding.QuerydslPredicate;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class WelcomeController {
+public class DummyModelController {
 
 
     @Autowired
@@ -26,5 +27,13 @@ public class WelcomeController {
         model.addAttribute("users", repository.findAll(predicate, pageable));
 
         return "table";
+    }
+
+    @RequestMapping(value = "/{id}")
+    String item(Model model, @PathVariable long id) {
+
+        model.addAttribute("user", repository.findOne(id));
+
+        return "item";
     }
 }
